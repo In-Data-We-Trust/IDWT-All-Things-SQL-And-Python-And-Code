81ab6a5b60161caa41c6b0b19b9390a311e4b141
pratik-IDWT <112123058+pratik-IDWT@users.noreply.github.com>
2023-09-22 16:00:34 +0100

    6 create an understanding of fpl data and create a python notebook to engineer it into azure (#7)
    * Updated FPL notebook with player data
    
    * updated FPL notebook to print less on notebook
    
    * updated headings in FPL notebook
    
    * updating FPL notebook extra steps
    
    * Added gameweek history function to FPL
    
    * updated FPL notebook
    
    * updated FPL notebook and completed
    
    * adding .gitignore file

---
+++ Python_Reference/database_connections_git_copy.py
@@ -0,0 +1,9 @@
+# The file needs to be renamed to database_connections_local.py
+# The variables need to be updated with server login information
+
+
+server_name = 'server_name'
+database = 'database'
+username = 'username'
+password = 'password'
+
